92a1a14811d69e2ccafc883b0103952dc3d12c2f
carlosnacero <carlosnacero7@gmail.com>
2024-06-26 18:26:52 -0400

    Login elements

--- cypress/e2e/Pages/login.elements.js
+++ cypress/e2e/Pages/login.elements.js
@@ -9,4 +9,16 @@ export class LoginElements {
    },
      };
     }
+
+
+    static get buttons(){
+     return {
+      get close(){
+       return cy.get('div[id="logInModal"] button').eq(1);
+  },
+     get login(){
+       return cy.contains('button', 'Log in');
+  },
+    };
+   }
 }
